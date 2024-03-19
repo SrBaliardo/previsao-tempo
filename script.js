615c9406f941b054cddeba5e7374e1978d879ba8
SrBaliardo <tao.john007@icloud.com>
2024-03-19 15:13:11 -0300

    correção de algumas responsividades e adição da função teclar Enter para pesquisar

--- script.js
+++ script.js
@@ -4,18 +4,24 @@ function showInfo(data) {
     console.log(data);
     document.querySelector('.weather-city').innerHTML = 'Tempo em ' + data.name;
     document.querySelector('.temperature').innerHTML = Math.floor(data.main.temp) + 'º C';
-    document.querySelector('.type-sky').innerHTML = data.weather[0].description;
+    document.querySelector('.sky').innerHTML = data.weather[0].description;
     document.querySelector('.humidity').innerHTML = 'Umidade: ' + data.main.humidity + '%';
     document.querySelector('.img-weather').src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
 
 }
 
- async function citySearch(city) {
-    const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${keyApi}&lang=pt_br&units=metric`).then (response => response.json());
-    showInfo(data);    
+async function citySearch(city) {
+    const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${keyApi}&lang=pt_br&units=metric`).then(response => response.json());
+    showInfo(data);
 }
 
 function clickSearch() {
     const city = document.querySelector('.input-city').value;
     citySearch(city);
 }
+
+document.addEventListener('keypress', function (event) {
+    if (event.key === 'Enter') {
+        clickSearch();
+    }
+});
