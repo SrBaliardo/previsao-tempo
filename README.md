e9c859152c6f1de11e06fe6dc86bf34354e4b2d0
SrBaliardo <tao.john007@icloud.com>
2025-01-22 16:00:20 -0300

    fix(code): README and background bug.

--- README.md
+++ README.md
@@ -1,16 +1,26 @@
+# Verifique o clima em qualquer lugar do mundo.
+
+Digite o nome da cidade sobre a qual você deseja obter informações e clique em pesquisar ou pressione “enter” para pesquisar. É isso aí! <br>
+
+Este usa o consumo de API para exibir informações.
+
+<br>
+
 # Check the weather anywhere in the world.
 
 Type in the name of the city you want information on and click on "search" or press "enter" to search. That's it! <br>
 
-This one uses API consumption to display information and images.
+This one uses API consumption to display information.
 
 <br>
 
 You can view the project via the link: <br>
 https://srbaliardo.github.io/previsao-tempo
 
+<img align="center" alt="HTML" height="400" width="500" src="./images/animation_previsao-tempo.gif">
+
 <br>
 
-" <img align="center" alt="HTML" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg"> | 
-  <img align="center" alt="CSS" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg"> | 
-  <img align="center" alt="Js" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-plain.svg"> "
+" <img align="center" alt="HTML" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg"> |
+<img align="center" alt="CSS" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg"> |
+<img align="center" alt="Js" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-plain.svg"> "
--- script.js
+++ script.js
@@ -1,29 +1,37 @@
-const keyApi = 'd7a994a009e010705536fa68bdf4b031';
-const inputSearch = document.querySelector('.input-city');
+const keyApi = "d7a994a009e010705536fa68bdf4b031";
+const inputSearch = document.querySelector(".input-city");
 
 function showInfo(data) {
-    console.log(data);
-    document.querySelector('.weather-city').innerHTML = 'Tempo em ' + data.name;
-    document.querySelector('.temperature').innerHTML = Math.floor(data.main.temp) + 'º C';
-    document.querySelector('.sky').innerHTML = data.weather[0].description;
-    document.querySelector('.humidity').innerHTML = 'Umidade: ' + data.main.humidity + '%';
-    document.querySelector('.img-weather').src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
-    inputSearch.value = '';
-
+  console.log(data);
+  document.querySelector(".weather-city").innerHTML = "Tempo em " + data.name;
+  document.querySelector(".temperature").innerHTML =
+    Math.floor(data.main.temp) + "º C";
+  document.querySelector(".temperature-feelslike").innerHTML =
+    "Sensação de " + Math.floor(data.main.feels_like) + "º C";
+  document.querySelector(".sky").innerHTML = data.weather[0].description;
+  document.querySelector(".humidity").innerHTML =
+    "Umidade: " + data.main.humidity + "%";
+  document.querySelector(
+    ".img-weather"
+  ).src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+  inputSearch.value = "";
 }
 
 async function citySearch(city) {
-    const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${keyApi}&lang=pt_br&units=metric`).then(response => response.json());
-    showInfo(data);
+  const data = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${keyApi}&lang=pt_br&units=metric`
+  ).then((response) => response.json());
+  showInfo(data);
+  console.log(data);
 }
 
 function clickSearch() {
-    const city = document.querySelector('.input-city').value;
-    citySearch(city);
+  const city = document.querySelector(".input-city").value;
+  citySearch(city);
 }
 
-document.addEventListener('keypress', function (event) {
-    if (event.key === 'Enter') {
-        clickSearch();
-    }
+document.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    clickSearch();
+  }
 });
